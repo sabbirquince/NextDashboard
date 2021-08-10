f890d632cc7ca5b0dccc123e872962c8de7cc817
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-08-10 17:07:05 +0600

    added head

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -2,10 +2,16 @@ import React from "react";
 import { dashboardData } from "../src/data/dashboardData";
 import Grid from "@material-ui/core/Grid";
 import CardDashboard from "../src/components/Card/CardDashboard";
+import Head from "next/head";
 
 export default function NewPage() {
   return (
     <div>
+      <Head>
+        <title>Dashboard</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
       <Grid container style={{ padding: "0 15px" }}>
         {dashboardData.map((each) => (
           <Grid key={each.id} item md={3}>
